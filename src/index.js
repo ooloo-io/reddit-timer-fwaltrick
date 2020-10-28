5fdd30bef23d94728cf66386a644fd06cf147b15
Fabricio Waltrick <fabricio.waltrick@gmail.com>
2020-10-28 21:23:33 +0100

    Added BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Normalize } from 'styled-normalize';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
 const Root = () => (
-  <>
+  <Router>
     <Normalize />
     <App />
-  </>
+  </Router>
 );
 
 // eslint-disable-next-line react/jsx-filename-extension
