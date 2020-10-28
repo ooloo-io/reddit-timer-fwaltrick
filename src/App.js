4234a8c92a7129cbae4a002c678a5712ee51f1d1
Fabricio Waltrick <fabricio.waltrick@gmail.com>
2020-10-29 00:25:58 +0100

    Fixed router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
+import { Normalize } from 'styled-normalize';
 import GlobalStyle from './style/GlobalStyle';
 import Search from './pages/Search';
 import Home from './pages/Home';
@@ -9,15 +10,18 @@ import theme from './style/Theme';
 function App() {
   return (
     <ThemeProvider theme={theme}>
+      <Normalize />
       <GlobalStyle />
-      <Switch>
-        <Route path="/search">
-          <Search />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
+      <Router>
+        <Switch>
+          <Route path="/search">
+            <Search />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </ThemeProvider>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Normalize } from 'styled-normalize';
-import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
-const Root = () => (
-  <Router>
-    <Normalize />
-    <App />
-  </Router>
-);
-
 // eslint-disable-next-line react/jsx-filename-extension
-ReactDOM.render(<Root />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
